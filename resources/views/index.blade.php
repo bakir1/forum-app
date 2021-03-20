11aa92b14571b7cfa0a604a480d14b8590daddbc
bakir1 <bakir_cutuk@hotmail.com>
2021-03-20 21:58:10 +0100

    send to other blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -58,9 +58,6 @@
     <body>
         <div class="flex-center position-ref full-height">
             <div class="content">
-                <div class="title m-b-md">
-                    My Application
-                </div>
 
                 @include('partials.nav')
             </div>
